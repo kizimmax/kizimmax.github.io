cac3c66212f6823541151c448ea7cbf7bf6d9f85
Максим Кизим <Maxi@MacBook-Pro.local>
2026-03-28 14:47:39 +0300

    feat: обновление сайта

--- src/components/sections/HeroSection.tsx
+++
@@ -1,9 +0,0 @@
-import { getPartnerLogos } from "@/lib/partner-logos";
-
-import { HeroSectionClient } from "./HeroSectionClient";
-
-export async function HeroSection() {
-    const logos = await getPartnerLogos();
-
-    return <HeroSectionClient logos={logos} />;
-}
